080f0f409d2238fc181610b584c04640181de223
João Santana <eventocrossx@gmail.com>
2026-03-30 22:15:49 -0300

    Add files via upload

---
+++ Modularização/19_new.py
@@ -0,0 +1,6 @@
+from mod19 import dif_2
+
+v1=int(input('Digite o valor: '))
+v2=int(input('Digite o valor: '))
+
+print (dif_2(v1, v2))
---
+++ Modularização/mod19.py
@@ -0,0 +1,5 @@
+def dif_2 (v1, v2):
+    if (v1>v2):
+        print (f'O maior é {v1}')
+    else: 
+        print (f'O maior é {v2}')
