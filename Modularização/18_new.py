722b73727cb97b8403723d28a2c393fcf5b9b2c4
João Santana <eventocrossx@gmail.com>
2026-03-30 21:55:26 -0300

    Add files via upload

---
+++ Modularização/18_new.py
@@ -0,0 +1,6 @@
+from mod18 import diferenca
+
+v1=int(input('Digite o primeiro valor: '))
+v2=int(input('Digite o segundo valor: '))
+
+print (diferenca(v1,v2))
---
+++ Modularização/mod18.py
@@ -0,0 +1,5 @@
+def diferenca(valor1, valor2):
+    Dif= (valor1-valor2)
+    if (Dif<0):
+        Dif= valor2-valor1 
+    return Dif
